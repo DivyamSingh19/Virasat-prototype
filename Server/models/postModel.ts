d20a56614ff15e66123a8215744a20cfde896634
Divyam Singh <divyamsingh1918@gmail.com>
2025-02-15 15:03:33 +0530

    Updates

--- Server/models/postModel.ts
+++ Server/models/postModel.ts
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const postSchema = new mongoose.Schema({
     name:{type:String,required:true},
     description:{type:String,required:true},
-    
+    image:{type:String,required:true},
+    date:{type:Number, required:true}
     
 
 })
