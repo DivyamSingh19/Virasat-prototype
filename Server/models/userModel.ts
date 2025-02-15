cebc8f4814cb8b7a9d11ce4c0e4e72e7f4906a6f
Divyam Singh <divyamsingh1918@gmail.com>
2025-02-15 19:24:21 +0530

    Updates

--- Server/models/userModel.ts
+++ Server/models/userModel.ts
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     name:{type:String,required:true},
     email:{type:String,required:true,unique:true},
-    password:{type:String,required:true}
-
+    password:{type:String,required:true},
+    role:{type:String,enum:["admin","student","alumni"],default:"user"}
 
 },{minimize:false})
 
